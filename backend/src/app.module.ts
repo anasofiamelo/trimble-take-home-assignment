0a9430774f07e2cb7015c3427f1e5741fc83b2bb
Ana Sofia <sofiamelod@gmail.com>
2023-04-15 17:26:09 -0300

    feat: add MongooseModule and TrucksModule to app module imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TrucksModule } from './trucks/trucks.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_DB_URI),
+    TrucksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
