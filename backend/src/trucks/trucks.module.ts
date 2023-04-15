dd796edd0801034126bcd4d4b562893b4eceb12a
Ana Sofia <sofiamelod@gmail.com>
2023-04-15 17:24:15 -0300

    feat: add Trucks MongooseModule to trucks module imports

--- backend/src/trucks/trucks.module.ts
+++ backend/src/trucks/trucks.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { TrucksService } from './trucks.service';
 import { TrucksController } from './trucks.controller';
 
+import { TrucksSchema } from './schemas/truck.schema';
+import { Trucks } from './schemas/truck.schema';
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Trucks.name, schema: TrucksSchema }]),
+  ],
   controllers: [TrucksController],
-  providers: [TrucksService]
+  providers: [TrucksService],
 })
 export class TrucksModule {}
