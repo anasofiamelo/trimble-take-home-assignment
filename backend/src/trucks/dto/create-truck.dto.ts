054e81aa833d5cefabe43024d9b9d6e4a1dfb051
Ana Sofia <sofiamelod@gmail.com>
2023-04-15 21:44:15 -0300

    feat: add fields and validation in create truck dto

--- backend/src/trucks/dto/create-truck.dto.ts
+++ backend/src/trucks/dto/create-truck.dto.ts
@@ -1,1 +1,14 @@
-export class CreateTruckDto {}
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+export class CreateTruckDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(17)
+  chassi: string;
+
+  @IsNotEmpty()
+  @IsString()
+  model: string;
+
+  @IsNotEmpty()
+  year: number;
+}
