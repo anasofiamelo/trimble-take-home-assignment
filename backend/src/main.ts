b719281cabadb2fb1deb68d70c8b12724568bd40
Ana Sofia <sofiamelod@gmail.com>
2023-04-15 21:43:46 -0300

    feat: add useGlobalPipes w/ ValidationPipe in main file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { config } from 'dotenv';
 config();
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.APP_PORT || 4000);
 }
 bootstrap();
