b93a9fed439fd040e40a54dcfd4e53e9e36638c8
Ana Sofia <sofiamelod@gmail.com>
2023-04-17 17:27:32 -0300

    refactor(frontend): delete App file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
-import App from './App'
 import { router } from './routes'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
