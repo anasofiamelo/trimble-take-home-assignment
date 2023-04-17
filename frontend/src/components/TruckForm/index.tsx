53b934d1daa3106f8f8d6423732f0d92e586a797
Ana Sofia <sofiamelod@gmail.com>
2023-04-17 17:23:37 -0300

    feat(frontend): create TruckForm component

---
+++ frontend/src/components/TruckForm/index.tsx
@@ -0,0 +1,91 @@
+import { FormEvent, useState } from 'react'
+import { TextField, Button, Alert } from '@mui/material'
+import { AxiosError } from 'axios'
+
+import TruckService from '../../services/TruckService'
+import useAlert from '../../hooks/useAlert'
+import { status } from '../../constants/httpStatusCode'
+
+function TruckForm() {
+  const [chassi, setChassi] = useState<string>('')
+  const [model, setModel] = useState<string>('')
+  const [year, setYear] = useState<string>('')
+
+  const missingData = !chassi || !model || !year
+
+  const { alert, setAlert, handleCloseAlert } = useAlert()
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    try {
+      setAlert(null)
+
+      e.preventDefault()
+
+      await TruckService.create({ chassi, model, year: +year })
+
+      setAlert({ type: 'success', message: 'Truck created' })
+    } catch (err: unknown) {
+      const error = err as AxiosError
+
+      const response = error.response
+
+      if (response?.status === status.conflict) {
+        setAlert({ type: 'warning', message: 'Truck already registered' })
+        return
+      }
+
+      setAlert({ type: 'error', message: 'Server error' })
+    }
+  }
+
+  return (
+    <div>
+      <h2>Add a truck</h2>
+      <form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 15
+        }}
+        onSubmit={handleSubmit}
+      >
+        {alert && (
+          <Alert onClose={handleCloseAlert} severity={alert?.type}>
+            {alert?.message}
+          </Alert>
+        )}
+        <div style={{ display: 'grid', gap: 10 }}>
+          <TextField
+            onChange={(e) => setChassi(e.target.value)}
+            label="Chassi"
+            size="small"
+            variant="outlined"
+            required={true}
+          />
+          <TextField
+            onChange={(e) => setModel(e.target.value)}
+            label="Model"
+            size="small"
+            variant="outlined"
+            required={true}
+          />
+          <TextField
+            onChange={(e) => setYear(e.target.value)}
+            label="Year"
+            size="small"
+            variant="outlined"
+            type="number"
+            required={true}
+          />
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Button disabled={missingData} type="submit" variant="contained">
+            Add truck
+          </Button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default TruckForm
