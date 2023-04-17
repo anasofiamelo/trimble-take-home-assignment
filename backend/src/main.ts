a1116dc4b3c7aeaf54b06e4fc926ea5e8fba9b8c
Ana Sofia <sofiamelod@gmail.com>
2023-04-17 18:49:34 -0300

    feat(backend): enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.APP_PORT || 4000);
 }
