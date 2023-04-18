6758dd9a22869a3d2bf8ae0c17d79f1d7723ad09
Ana Sofia <sofiamelod@gmail.com>
2023-04-18 14:07:54 -0300

    feat(frontend): implement delete function in TruckCard

--- frontend/src/components/TrucksList/TruckCard/index.tsx
+++ frontend/src/components/TrucksList/TruckCard/index.tsx
@@ -1,20 +1,53 @@
-import { Button, Card, CardActions, CardContent, Typography, Stack } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { AxiosError } from 'axios'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+  Stack,
+  Alert
+} from '@mui/material'
+
+import TruckService from '../../../services/TruckService'
+import useAlert from '../../../hooks/useAlert'
 
 function TruckCard(props: any) {
-  const { model, chassi, year } = props
+  const { _id, model, chassi, year } = props
+
+  const { alert, setAlert, handleCloseAlert } = useAlert()
+
+  async function handleDeleteTruck() {
+    try {
+      await TruckService.delete(_id)
+    } catch (err: unknown) {
+      const error = err as AxiosError
+      if (!error.response) {
+        setAlert({ type: 'error', message: 'Internal server error' })
+      }
+    }
+  }
   return (
-    <Card variant="outlined">
+    <Card sx={{ padding: 1 }} variant="outlined">
+      {alert && (
+        <Alert onClose={handleCloseAlert} severity={alert.type}>
+          {alert.message}
+        </Alert>
+      )}
       <CardContent>
         <Stack direction="row" alignItems="center" spacing={0.5}>
           <Typography color="text.secondary">Model: </Typography>
           <Typography variant="h6">{model.toUpperCase()}</Typography>
         </Stack>
+
         <Stack direction="row" alignItems="center" spacing={0.5}>
           <Typography sx={{ fontSize: 14 }} color="text.secondary">
             Chassi:
           </Typography>
           <Typography>{chassi}</Typography>
         </Stack>
+
         <Stack direction="row" alignItems="center" spacing={0.5}>
           <Typography sx={{ fontSize: 14 }} color="text.secondary">
             Year:
@@ -22,8 +55,20 @@ function TruckCard(props: any) {
           <Typography>{year}</Typography>
         </Stack>
       </CardContent>
-      <CardActions>
-        <Button size="small">Delete truck</Button>
+      <CardActions
+        sx={{
+          display: 'flex',
+          alignItems: 'flex-end',
+          justifyContent: 'space-between'
+        }}
+      >
+        <Link to={`${_id}`}>
+          <Button size="small">Truck details</Button>
+        </Link>
+
+        <Button onClick={handleDeleteTruck} size="small" variant="contained">
+          Delete truck
+        </Button>
       </CardActions>
     </Card>
   )
