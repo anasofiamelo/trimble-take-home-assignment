7cefbbd59fea012cd1193d654106e2f1605a0a50
Ana Sofia <sofiamelod@gmail.com>
2023-04-18 12:54:17 -0300

    feat(frontend): TruckDetails component: use state to track the map center (lastLocation), add a Button to /add-last-location route, implement Map component and update return from loader function

--- frontend/src/components/TruckDetails/index.tsx
+++ frontend/src/components/TruckDetails/index.tsx
@@ -1,6 +1,8 @@
-import { useEffect } from 'react'
-import { useLoaderData } from 'react-router-dom'
-import { Alert } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { Link, useLoaderData } from 'react-router-dom'
+import { Alert, Button, Stack } from '@mui/material'
+import { LatLngExpression } from 'leaflet'
+import { AxiosError } from 'axios'
 
 import { status } from '../../constants/httpStatusCode'
 
@@ -10,6 +12,8 @@ import useAlert from '../../hooks/useAlert'
 
 import { Truck } from '../../interfaces/Truck'
 
+import Map from './Map'
+
 type LoaderData = {
   truck?: Truck
   status?: number
@@ -17,23 +21,34 @@ type LoaderData = {
 
 function TruckDetails() {
   const data = useLoaderData() as LoaderData
+  const lastLocation = data?.truck?.lastLocation
+
   const { alert, setAlert, handleCloseAlert } = useAlert()
+  const [center, setCenter] = useState<LatLngExpression>([-3.7232, -38.4701])
 
   useEffect(() => {
     if (data.status === status.internal_server_error) {
-      setAlert({ type: 'error', message: 'Erro no servidor interno' })
+      setAlert({ type: 'error', message: 'Internal server error' })
+    }
+    if (lastLocation) {
+      setCenter([lastLocation.latitude, lastLocation.longitude])
     }
   }, [data])
 
   return (
     <div>
-      <h2>Truck</h2>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <h2>Truck</h2>
 
-      {alert && (
-        <Alert onClose={handleCloseAlert} severity={alert.type}>
-          {alert.message}
-        </Alert>
-      )}
+        {alert && (
+          <Alert onClose={handleCloseAlert} severity={alert.type}>
+            {alert.message}
+          </Alert>
+        )}
+        <Link to="add-last-location">
+          <Button variant="contained">Add Last Location</Button>
+        </Link>
+      </Stack>
 
       {data.truck && (
         <div>
@@ -42,15 +57,26 @@ function TruckDetails() {
           <p>Year: {data.truck.year}</p>
         </div>
       )}
+
+      {!lastLocation && (
+        <p>Looks like this truck does not have any locations registered yet.</p>
+      )}
+      <Map hasMarker={!!lastLocation} center={center} />
     </div>
   )
 }
 
 export async function loader({ params }: any) {
   try {
-    const response = await TruckService.findOne(params.chassi)
-    return { truck: response.data }
-  } catch (error: any) {
+    const truck_response = await TruckService.findOne(params.id)
+    return {
+      truck: {
+        ...truck_response.data.truck,
+        lastLocation: truck_response.data.lastLocation[0]
+      }
+    }
+  } catch (err: unknown) {
+    const error = err as AxiosError
     if (!error.response) {
       return { status: status.internal_server_error }
     }
