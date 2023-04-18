d59d3a9be3e12fde302fa1558cc96e05c43e5b7f
Ana Sofia <sofiamelod@gmail.com>
2023-04-18 17:55:09 -0300

    refactor(backend): apply hard coded app port in main file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,12 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+const APP_PORT = 8080;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.APP_PORT || 4000);
+  await app.listen(APP_PORT);
 }
 bootstrap();
