c81e87dc16608c0fa2310a4aefd4f822eee08767
Ana Sofia <sofiamelod@gmail.com>
2023-04-18 13:13:55 -0300

    feat(frontend): routes: implement Root component as main element in router and implement AddLastLocation route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,15 +1,17 @@
-import { Outlet, createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import { routes } from '../constants/routes'
 
 // components
 import TrucksList, { loader as trucksLoader } from '../components/TrucksList'
 import TruckForm from '../components/TruckForm'
 import TruckDetails, { loader as truckDetailsLoader } from '../components/TruckDetails'
+import AddLastLocation from '../components/TruckDetails/AddLastLocation'
+import Root from '../components/Root'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Outlet />,
+    element: <Root />,
     children: [
       {
         path: routes.trucks_list,
@@ -23,6 +25,11 @@ export const router = createBrowserRouter([
       {
         path: routes.truck_details,
         element: <TruckDetails />,
+        loader: truckDetailsLoader
+      },
+      {
+        path: routes.add_last_location,
+        element: <AddLastLocation />,
         loader: truckDetailsLoader
       }
     ]
