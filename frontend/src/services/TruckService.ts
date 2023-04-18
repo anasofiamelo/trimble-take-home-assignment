fc4cfe3e892ff620e07011d8e882b64c3be697b3
Ana Sofia <sofiamelod@gmail.com>
2023-04-18 12:45:38 -0300

    feat(frontend): transform TruckService into an extended version from BaseService with location methods

--- frontend/src/services/TruckService.ts
+++ frontend/src/services/TruckService.ts
@@ -1,66 +1,16 @@
-import axios, { AxiosHeaders } from 'axios'
-import { Truck } from '../interfaces/Truck'
-
-type HttpMethods = 'post' | 'get' | 'put' | 'patch' | 'delete'
-
-const resolve = (args: string[]) => {
-  const formattedArgs: string[] = []
-
-  args.forEach((arg) => {
-    if (!arg || arg === '/') return
-    formattedArgs.push(arg)
-  })
-
-  return formattedArgs.join('/')
-}
-
-export const BASE_URL = 'http://localhost:8080'
-
-class TruckService {
+import BaseService from './BaseService'
+class TruckService extends BaseService {
   static base_endpoint = '/trucks'
-  static base_url = BASE_URL
-  static headers: Partial<AxiosHeaders> = {}
 
-  static _create() {
-    return axios.create({
-      baseURL: this.base_url,
-      headers: this.headers
-    })
+  static async findLastLocation(id: string) {
+    await this.get(`${id}/locations/last`)
   }
 
-  static addHeaders(name: string, value: string) {
-    this.headers[name] = value
-  }
-
-  static async _fetch(endpoint: string, method = 'get', options = {}) {
-    const instance = this._create()
-    const url = resolve([this.base_endpoint, endpoint])
-    const httpMethod = method.toLowerCase() as HttpMethods
-    return await instance[httpMethod](url, options)
-  }
-
-  static get(endpoint: string, params: any) {
-    return this._fetch(endpoint, 'get', params)
-  }
-
-  static post(endpoint: string, params: any) {
-    return this._fetch(endpoint, 'post', params)
-  }
-
-  static findAll(params: any = {}) {
-    return this._fetch('', 'get', { params })
-  }
-
-  static findOne(id: string) {
-    return this._fetch(`${id}`, 'get')
-  }
-
-  static create(data: Truck) {
-    return this._fetch(`/`, 'post', data)
-  }
-
-  static delete(id: string) {
-    return this._fetch(`${id}`, 'delete')
+  static async createLocation(
+    truckId: string,
+    data: { chassis: string; latitude: number; longitude: number }
+  ) {
+    await this.post(`${truckId}/locations`, data)
   }
 }
 
