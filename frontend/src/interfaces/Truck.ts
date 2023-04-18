cbe87802040fef4271d3d1c8a3606914bbea67ff
Ana Sofia <sofiamelod@gmail.com>
2023-04-18 12:46:37 -0300

    feat(frontend): update Truck interface

--- frontend/src/interfaces/Truck.ts
+++ frontend/src/interfaces/Truck.ts
@@ -1,5 +1,14 @@
+export interface Location {
+  truckId: string
+  chassi: string
+  latitude: number
+  longitude: number
+}
+
 export interface Truck {
   chassi: string
   model: string
   year: number
+  lastLocation: Location
+  locations: Location[]
 }
